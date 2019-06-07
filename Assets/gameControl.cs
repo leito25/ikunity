fece186f1b6c5d81e1dc1eb9dba9f6d80043c6d5
IELeonardoQuignones2015 <lquinones@innovativecolombia.com>
2019-06-07 11:58:08 -0500

    Project Files

---
+++ Assets/gameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameControl : MonoBehaviour
+{
+
+    public Animator my_animator;//objeto animador
+    public GameObject my_gameCharacter;// el objeto Rbt(dentro tiene el componente transform)
+
+    //Estados de la animación
+    bool idle = true;
+    bool run = false;
+    bool attack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mirar a la Izquierda
+        if (Input.GetButton("LeftSide"))
+        {
+
+            my_gameCharacter.transform.localScale = new Vector3(-1F, this.transform.localScale.y, this.transform.localScale.z);
+
+        }else if (Input.GetButton("RightSide")){//mirar a la derecha
+
+            my_gameCharacter.transform.localScale = new Vector3(1F, this.transform.localScale.y, this.transform.localScale.z);
+
+        }
+
+        //estados con cada botòn del mouse
+        if(Input.GetMouseButtonDown(0)){
+            run = !run;
+            my_animator.SetBool("Run", run);
+
+        }else if (Input.GetMouseButtonDown(1))
+        {
+            attack = !attack;
+            my_animator.SetBool("Attack", attack);
+        }
+
+    }
+}
